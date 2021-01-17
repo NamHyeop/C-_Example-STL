4be70520e59705816af101238b4387886b75059e
NamHyeop Kim <skaguq@gmail.com>
2021-01-17 12:47:00 +0900

    Update next_permutation.cpp

--- next_permutation.cpp
+++ next_permutation.cpp
@@ -6,24 +6,20 @@
 
 using namespace std;
 
-int main(){
-
+int main()
+{
 	vector<int> v(4); //테스트 백터 선언(밑에 사용법시 백터 초기화 필수)
  
 	for (int i = 0; i<4; i++) //백터값 지정
-  {
 		v[i] = i + 1;
-	}
-
+	
 	// next_permutation을 사용하여 순열 구하기
 	do
-  {
+	{
 		for (int i = 0; i<4; i++)
-    {
 			cout << v[i] << " ";
-		}
-
 		cout << '\n';
-	} while (next_permutation(v.begin(), v.end()));
+	}while(next_permutation(v.begin(), v.end()));
+	
 	return 0;
 }
