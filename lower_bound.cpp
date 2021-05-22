242a174d4390fce53f89f5c84d8d84bf02df5906
NamHyeop Kim <skaguq@gmail.com>
2021-05-22 15:02:52 +0900

    Create lower_bound.cpp

---
+++ lower_bound.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int nh_lower_bound(int *arr, int len, int num)
+{
+	int start = 0;
+	int end = len;
+
+	while (end - start > 0)
+	{
+		int mid = (start + end) / 2;
+		if (arr[mid] < num)
+			start = mid + 1;
+		else
+			end = mid;
+	}
+	return end;
+}
+
+int main(void){
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 8, 9 };
+
+	cout << "lower_bound(3) : " << lower_bound(arr, arr + 10, 3) - arr << endl;
+	cout << "lower_bound(4) : " << lower_bound(arr, arr + 10, 4) - arr << endl;
+	cout << "lower_bound(8) : " << lower_bound(arr, arr + 10, 8) - arr<< endl;
+	cout << "lower_bound(9) : " << lower_bound(arr, arr + 10, 9) - arr<< endl;
+	cout << "==================================================="<< endl;
+	cout << "nh_lower_bound(3) : " << nh_lower_bound(arr, 10, 3) << endl;
+	cout << "nh_lower_bound(4) : " << nh_lower_bound(arr, 10, 4) << endl;
+	cout << "nh_lower_bound(8) : " << nh_lower_bound(arr, 10, 8) << endl;
+	cout << "nh_lower_bound(9) : " << nh_lower_bound(arr, 10, 9) << endl;
+  
+	return 0;
+}
